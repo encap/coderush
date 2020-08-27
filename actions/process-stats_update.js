0235c51cbdd01194ae9b7b89126f495dc896f342
encap <encapsulation4@gmail.com>
2020-08-27 18:06:53 +0200

    stats update handles fixes

--- actions/process-stats_update.js
+++ actions/process-stats_update.js
@@ -2,13 +2,12 @@
 /* eslint-disable import/no-dynamic-require */
 const fs = require('fs');
 
-console.log(process.env.GITHUB_EVENT_PATH);
-
 const { client_payload } = require(process.env.GITHUB_EVENT_PATH);
+
 
 console.log(client_payload);
 
-const list = client_payload.list;
+const list = client_payload;
 
 fs.writeFileSync(
   'public/list.json',
