a650441895af0a51a78179011474b570abb95e0f
encap <encap@users.noreply.github.com>
2020-10-02 20:39:01 +0000

    [create-pull-request] automated change

---
+++ public/code/JavaScript/jsonParseTest.js
@@ -0,0 +1,44 @@
+close"
+"arbitraryJSON": "hahaha"
+"database" : [
+    {
+      "ext": "ts",
+      "files": [
+        {
+          "lines": 23,
+          "name": "example_component",
+          "source": "freecodecamp.org",
+          "tabSize": 2
+        }
+      ],
+      "mode": "javascript",
+      "name": "Angular"
+    },
+    {
+      "ext": "asm",
+      "files": [
+        {
+          "lines": 24,
+          "name": "disk_read",
+          "source": "github.com/ghaiklor",
+          "tabSize": 8
+        }
+      ],
+      "mode": "z80",
+      "name": "Assembly"
+    },
+    {
+      "ext": "sh",
+      "files": [
+        {
+          "lines": 27,
+          "name": "internet_test",
+          "source": "own",
+          "tabSize": 4
+        }
+      ],
+      "mode": "clike",
+      "name": "Bash"
+    }
+],
+"lastKey": "unclose
